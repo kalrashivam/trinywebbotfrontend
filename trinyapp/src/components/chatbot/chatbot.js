301e63f896eae421a92dfb27f99fb6fd7f350304
engineersworkbook <shivamkalra2013@gmail.com>
2018-11-26 01:02:14 +0530

    chatbot viible

--- trinyapp/src/components/chatbot/chatbot.js
+++ trinyapp/src/components/chatbot/chatbot.js
@@ -1,27 +1,18 @@
 import React, {Component} from 'react';
 import axios from "axios/index";
+import Message from './message';
 
 class Chatbot extends Component {
     constructor(props){
         super(props)
-
+        this._handleInputKeyPress = this._handleInputKeyPress.bind(this);
         this.state= {
             messages: []
         }
     }
 
 
-    render() {
-        return(
-            <div style={{ height: 400, width:400, float: 'right'}}>
-                <div id="chatbot"  style={{ height: '100%', width:'100%', overflow: 'auto'}}>
-                    <h2>Chatbot</h2>
-                    Here we will display messages
-                </div>
-                <input type="text" placeholder="type your request here"  />
-            </div>
-        )
-    }  
+    
     
     async df_text_query (queryText) {
         let msg;
@@ -35,6 +26,7 @@ class Chatbot extends Component {
         }
         this.setState({ messages: [...this.state.messages, says]});
         const res = await axios.post('http://localhost:5000/api/df_text_query',  {text: queryText});
+        console.log(res)
          if (res.data.fulfillmentMessages ) {
             for (let i = 0; i < res.data.fulfillmentMessages.length; i++) {
                 msg = res.data.fulfillmentMessages[i];
@@ -60,6 +52,43 @@ class Chatbot extends Component {
             }
         }
     };
+
+
+    componentDidMount() {
+        this.df_event_query('Welcome');
+    }
+     _handleInputKeyPress(e) {
+        if (e.key === 'Enter') {
+            this.df_text_query(e.target.value);
+            e.target.value = '';
+        }
+    }
+
+
+    renderMessages(returnedMessages) {
+        if (returnedMessages) {
+            return returnedMessages.map((message, i) => {
+                    return <Message key={i} speaks={message.speaks} text={message.msg.text.text}/>;
+                }
+            )
+        } else {
+            return null;
+        }
+    }
+
+
+    render() {
+        return(
+            <div style={{ height: 400, width:400, float: 'right'}}>
+                <div id="chatbot"  style={{ height: '100%', width:'100%', overflow: 'auto'}}>
+                    <h2>Chatbot</h2>
+                    {this.renderMessages(this.state.messages)}
+                </div>
+                <input type="text"  />
+                <input type="text" onKeyPress={this._handleInputKeyPress} />
+            </div>
+        )
+    }  
 
     
    

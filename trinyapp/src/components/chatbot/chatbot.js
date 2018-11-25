ec999e4b2fb3e2fe871460dfb96ea472a9781f84
engineersworkbook <shivamkalra2013@gmail.com>
2018-11-26 01:48:03 +0530

    added unique id

--- trinyapp/src/components/chatbot/chatbot.js
+++ trinyapp/src/components/chatbot/chatbot.js
@@ -1,14 +1,23 @@
 import React, {Component} from 'react';
 import axios from "axios/index";
 import Message from './message';
+import Cookies from 'universal-cookie';
+import { v4 as uuid } from 'uuid';
+
+const cookies = new Cookies();
 
 class Chatbot extends Component {
+    messagesEnd;
     constructor(props){
         super(props)
         this._handleInputKeyPress = this._handleInputKeyPress.bind(this);
         this.state= {
             messages: []
         }
+        if (cookies.get('userID') === undefined) {
+            cookies.set('userID', uuid(), { path: '/' });
+        }
+        console.log(cookies.get('userID'))
     }
 
 
@@ -25,7 +34,7 @@ class Chatbot extends Component {
             }
         }
         this.setState({ messages: [...this.state.messages, says]});
-        const res = await axios.post('http://localhost:5000/api/df_text_query',  {text: queryText});
+        const res = await axios.post('http://localhost:5000/api/df_text_query',  {text: queryText,userID: cookies.get('userID')});
         console.log(res)
          if (res.data.fulfillmentMessages ) {
             for (let i = 0; i < res.data.fulfillmentMessages.length; i++) {
@@ -39,7 +48,7 @@ class Chatbot extends Component {
         }
     };
      async df_event_query(eventName) {
-         const res = await axios.post('http://localhost:5000/api/df_event_query',  {event: eventName});
+         const res = await axios.post('http://localhost:5000/api/df_event_query',  {event: eventName, userID: cookies.get('userID')});
         let msg, says = {};
          if (res.data.fulfillmentMessages ) {
             for (let i=0; i<res.data.fulfillmentMessages.length; i++) {
@@ -57,6 +66,11 @@ class Chatbot extends Component {
     componentDidMount() {
         this.df_event_query('Welcome');
     }
+
+    componentDidUpdate() {
+        this.scrollToBottom();
+    }
+
      _handleInputKeyPress(e) {
         if (e.key === 'Enter') {
             this.df_text_query(e.target.value);
@@ -83,12 +97,19 @@ class Chatbot extends Component {
                 <div id="chatbot"  style={{ height: '100%', width:'100%', overflow: 'auto'}}>
                     <h2>Chatbot</h2>
                     {this.renderMessages(this.state.messages)}
+                    <div style={{ float:"left", clear: "both" }}
+                         ref={(el) => { this.messagesEnd = el; }}>
+                    </div>
                 </div>
                 <input type="text"  />
                 <input type="text" onKeyPress={this._handleInputKeyPress} />
             </div>
         )
     }  
+
+    scrollToBottom = () => {
+        this.messagesEnd.scrollIntoView({ behavior: "smooth" });
+    }
 
     
    
